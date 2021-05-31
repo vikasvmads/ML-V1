8de47b582ada9bd4a15d2505ed1636a740a1ed63
Vikas Mishra <vikasmishra227.com>
2021-05-31 18:22:04 +0530

    added build folder

---
+++ build/static/js/6.c77fb796.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpml-project"]=this["webpackJsonpml-project"]||[]).push([[6],{214:function(n,c){},270:function(n,c){},271:function(n,c){}}]);
+//# sourceMappingURL=6.c77fb796.chunk.js.map
